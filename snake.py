cb95ab63755950f414c7eacbd6f4dbf6b609ca93
olafyang <ian9703@gmail.com>
2020-11-23 08:59:12 +0100

    update snake movement

--- snake.py
+++ snake.py
@@ -1,9 +1,9 @@
 import pygame
-import time
+import random
 
 display_size = (800, 800)
 snake_size = 20
-snake_speed = 5  # in pixel
+snake_speed = 10  # in pixel
 
 pygame.init()
 
@@ -21,9 +21,6 @@ lose_font_style = pygame.font.SysFont(name='loma', size=50)
 score_font_style = pygame.font.SysFont(name='loma', size=10)
 
 
-# def append_to_snake():
-
-
 def show_score(score):
     score_msg = lose_font_style.render('score: {}'.format(score), True, white)
     dis.blit(score_msg, [0, 0])
@@ -35,28 +32,47 @@ def lose_message(msg, color):
     pygame.display.update()
 
 
+def update_display(pos_list, score):
+    dis.fill(black)
+
+    # draw
+    for item in pos_list:
+        pygame.draw.rect(dis, white, [item[0], item[1], snake_size, snake_size])
+
+    show_score(score)
+    pygame.display.update()
+
+
+def generate_food():
+    food_pos = [round(random.randrange(1, display_size[0])), round(random.randrange(1, display_size[1]))]
+    pygame.draw.rect(dis, blue, [food_pos[0], food_pos[1], snake_size, snake_size])
+    pygame.display.update()
+
+
 def game_loop():
     player_score = 0
 
     game_over = False
     quit_game = False
 
-    snake_list = []
+    snake_pos_list = []
 
     x1 = int(display_size[0] / 2 - snake_size)
     y1 = int(display_size[1] / 2 - snake_size)
-    snake_list.append([x1, y1])
-
+    snake_pos_list.append([x1, y1])
     v_x1 = 0
     v_y1 = 0
 
     while not quit_game:
+        # handle input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_game = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    snake_list.append([snake_list[len(snake_list) - 1][0] + v_x1, snake_list[len(snake_list) - 1][1] + v_y1])
+                    # code to extend length
+                    snake_pos_list.append(
+                        [snake_pos_list[0][0], snake_pos_list[0][1]])
                     player_score += 1
                     continue
                 v_x1 = 0
@@ -69,31 +85,28 @@ def game_loop():
                     v_x1 = snake_speed
                 elif event.key == pygame.K_DOWN:
                     v_y1 = snake_speed
+
         if not game_over:
-            snake_list[0][0] += v_x1
-            snake_list[0][1] += v_y1
-            for i, item in enumerate(snake_list):
-                if i == 0:
-                    continue
-                else:
-                    snake_list[i][0] = snake_list[i - 1][0] - v_x1
-                    snake_list[i][1] = snake_list[i - 1][1] - v_y1
+            # extend snake length
+            snake_pos_list[0][0] += v_x1
+            snake_pos_list[0][1] += v_y1
 
-            dis.fill(black)
-            for item in snake_list:
-                pygame.draw.rect(dis, white, [item[0], item[1], snake_size, snake_size])
-            # pygame.draw.rect(dis, white, [snake_list[0][0], snake_list[0][1], snake_size, snake_size])
-
-            show_score(player_score)
-
-            pygame.display.update()
+            snake_pos_list.append([snake_pos_list[0][0], snake_pos_list[0][1]])
+            update_display(snake_pos_list, player_score)
+            if len(snake_pos_list) > player_score:
+                snake_pos_list.pop(1)
 
             clock.tick(60)
 
         # detect if snake hits boundaries
-        if snake_list[0][0] >= display_size[0] or snake_list[0][0] < 0 or snake_list[0][1] >= display_size[1] or snake_list[0][1] < 0:
+        if snake_pos_list[0][0] >= display_size[0] or snake_pos_list[0][0] < 0 or snake_pos_list[0][1] >= display_size[1] or snake_pos_list[0][1] < 0:
             game_over = True
             lose_message('Game Over', red)
+        # detect if snake hits itself
+        # for item in snake_pos_list[1:]:
+        #     if snake_pos_list[0][0] == item[0] and snake_pos_list[0][1] == item[1]:
+        #         game_over = True
+        #         lose_message('Game Over', red)
 
     pygame.quit()
     quit()
